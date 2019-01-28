79956cedb26169e7f662185554c0d67483d5974e
DESKTOP-T3KR0E0\Undefined <xx.bryan.xx@msn.com>
2019-01-28 10:53:01 -0500

    fixed export schema

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ class GestionDB{
 
    // conexion credentials
    private $datos = array( 
-					      "host" => "localhost", 
-					      "user" => "laravel",
-					      "pass" => "Newm00n**"
-					    );
+                "host" => "localhost", 
+                "user" => "username",
+                "pass" => "password"
+              );
 
     //esclude databases squema
-   	private $dbe = ["information_schema","mysql","performance_schema","phpmyadmin"];
-   	
-   	// databases select
-   	private $dbs;
+    private $dbe = ["information_schema","mysql","performance_schema","phpmyadmin"];
+    
+    // databases select
+    private $dbs;
 
-   	// mysqli instance
+    // mysqli instance
     private static $_mysqli;
 
     // singleton instance
@@ -28,12 +28,12 @@ class GestionDB{
     private $msg_errors = ["web" => "<b><h1 style='color:red;'> %s </h1></b><br>", "cli" => "%s\n" ]; 
 
     private function __construct($database=false){
-     	//detect sapi
-     	$this->interface = php_sapi_name() == 'cli' ? "cli" : "web";
+      //detect sapi
+      $this->interface = php_sapi_name() == 'cli' ? "cli" : "web";
 
      try{
-     	// instance datatabase
-    	self::$_mysqli = new \mysqli($this->datos['host'],$this->datos['user'], $this->datos['pass']); 
+      // instance datatabase
+      self::$_mysqli = new \mysqli($this->datos['host'],$this->datos['user'], $this->datos['pass']); 
      
       if (self::$_mysqli->connect_error) {
             throw new Exception(sprintf($this->msg_errors[$this->interface],'Connect Error ' . self::$_mysqli->connect_errno . ': ' . self::$_mysqli->connect_error, self::$_mysqli->connect_errno));
@@ -65,10 +65,10 @@ class GestionDB{
 
     // function multiple query -> insert
     public function CM($sql){
-    	$consulta = self::$_mysqli->multi_query($sql);
-    	if(!$consulta)
-    		die(sprintf($this->msg_errors[$this->interface],self::$_mysqli->error));
-    	return true;
+      $consulta = self::$_mysqli->multi_query($sql);
+      if(!$consulta)
+        die(sprintf($this->msg_errors[$this->interface],self::$_mysqli->error));
+      return true;
     }
 
     // function query return -> select
@@ -87,126 +87,141 @@ class GestionDB{
 
     //validate file
     private function CD($file){
-		
-		//validate exists
-		if (!file_exists($file)) {
-			die(sprintf($this->msg_errors[$this->interface],"no existe la ruta o el archivo!"));
-		}
+    
+    //validate exists
+    if (!file_exists($file)) {
+      die(sprintf($this->msg_errors[$this->interface],"no existe la ruta o el archivo!"));
+    }
 
-		// validate Permissions
-		if (!is_readable($file)) {
-			die(sprintf($this->msg_errors[$this->interface],"no se puede leer la ruta o el archivo!"));
-		}
+    // validate Permissions
+    if (!is_readable($file)) {
+      die(sprintf($this->msg_errors[$this->interface],"no se puede leer la ruta o el archivo!"));
+    }
 
-		return true;
+    return true;
     }
 
     // drop databases only test script
     public function DropDB($databases=false){
-    	if(!$databases){
-			foreach ($this->dbs as $db) {
-				self::CS('DROP DATABASE '.$db.";");
-			}
-		}else{
+      if(!$databases){
+      foreach ($this->dbs as $db) {
+        self::CS('DROP DATABASE '.$db.";");
+      }
+    }else{
 
-			$dbs = is_array($databases) ? $databases : explode(',',$databases);
-			foreach ($dbs as $db) {
-				self::CS('DROP DATABASE '.$db.";");
-			}
-		}
-		return true;
+      $dbs = is_array($databases) ? $databases : explode(',',$databases);
+      foreach ($dbs as $db) {
+        self::CS('DROP DATABASE '.$db.";");
+      }
+    }
+    return true;
     }
 
     // restore databasess 
-    public function Restore($sqlfilename,$database=false){
+    public function Restore($filename,$database=false){
 
-    	// restore specific database;
-    	if($database){
-    		self::CS("CREATE DATABASE IF NOT EXISTS ".$database);
-    		self::CS("use ".$database);
-    	}
+      // restore specific database;
+      if($database){
+        self::CS("CREATE DATABASE IF NOT EXISTS ".$database);
+        self::CS("use ".$database);
+      }
 
-    	// check file name
- 		self::CD($sqlfilename);
+      // check file name
+    self::CD($sqlfilename);
 
-		// load file
-		$sql = file_get_contents($sqlfilename);
+    // load file
+    $sql = file_get_contents($filename);
 
-		//execute file
-		return self::CM($sql);
+    //execute file
+    return self::CM($sql);
     }
 
     // backup databases;
-    public function Backup($sqlfilename=false,$databases = '*'){
-      
-	      $return="";
+    public function Backup($filename=false,$databases = '*'){
 
-	      if($sqlfilename == false){
-	      	 $sqlfilename = "dumpsql-".date('ymd').".sql";
-	      }
+        //all databases
+        if($databases == '*'){
+          $dbs = array();
+          $result = self::CR('SHOW DATABASES');
+          while($row = $result->fetch_row()){
+            if(!in_array($row[0], $this->dbe))
+              $dbs[] = $row[0];
+          }
+        }
+        else{
+          $dbs = is_array($databases) ? $databases : explode(',',$databases);
+        }
 
-	      //all databases
-	      if($databases == '*'){
-	        $dbs = array();
-	        $result = self::CR('SHOW DATABASES');
-	        while($row = $result->fetch_row()){
-	          if(!in_array($row[0], $this->dbe))
-	          	$dbs[] = $row[0];
-	        }
-	      }
-	      else{
-	        $dbs = is_array($databases) ? $databases : explode(',',$databases);
-	      }
+        if(empty($dbs))
+          die(sprintf($this->msg_errors[$this->interface],"ups! 0 databases select for export!"));
 
-		if(empty($dbs))
-			die(sprintf($this->msg_errors[$this->interface],"ups! 0 databases select for export!"));
+        // filename is set no rewrite file if database is 2 or more 
+        if(count($dbs) > 1 && $filename){
+            $filename = str_replace(".sql", "-%s.sql",$filename);
+        }
 
-	    $this->dbs=$dbs;
+        // default export filename
+        if($filename== false){
+            $filename = "dump-".date('ymd')."-%s.sql";
+        }
 
-	    foreach($dbs as $db){
+      $this->dbs=$dbs;
 
-	    	$tables = array();
-	    	if($databases == '*'){
-	    		$return.= "CREATE DATABASE IF NOT EXISTS ". $db.";\n";
-		    	$return.= "USE ". $db.";\n";
-	    	}
+      foreach($dbs as $db){
 
-		    $result = self::CR('SHOW TABLES IN '.$db);
+        // header file
+        $ip = $_SERVER['SERVER_ADDR']!='::1' ? $_SERVER['SERVER_ADDR'] : '127.0.0.1';
+        $return = "-- php export database\n";
+        $return .= "-- version 1.4\n";
+        $return .= "-- Servidor: {$ip}\n";
+        $return .= "-- Tiempo de generación: ".date('d-m-Y')." a las ".date('H:i:s')."\n";
+        $return .= "-- Versión del servidor: ".self::CR('SELECT VERSION()')->fetch_row()[0]."\n";
+        $return .= "-- Versión de PHP: ".phpversion()."\n\n\n";
+        $return .= "--\n-- Base de datos: ${db}\n--\n";
 
-		    //all tables
-		    while($row = $result->fetch_row()){
-		    	$tables[]= $row[0];
-		    }
+        $tables = array();
 
-		    foreach($tables as $table){
-		        $result = self::CR('SELECT * FROM '. $db. '.'.$table);
-		        $num_fields = $result->field_count;
-		        
-		        $return.= 'DROP TABLE IF EXISTS '.$table.';';
-		        $qr=self::CR('SHOW CREATE TABLE '.$db.'.'.$table);
-		        $row2 = $qr->fetch_row();
-		        $return.= "\n\n".$row2[1].";\n\n";
-		      
-		        for ($i = 0; $i < $num_fields; $i++) {
-		          while($row = $result->fetch_row()){
-		            $return.= 'INSERT INTO '.$table.' VALUES(';
-		            for($j=0; $j < $num_fields; $j++) {
-		              $row[$j] = addslashes($row[$j]);
-		              $row[$j] = preg_replace("#\n#","\\n",$row[$j]);
-		              if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
-		              if ($j < ($num_fields-1)) { $return.= ','; }
-		            }
-		            $return.= ");\n";
-		          }
-		        }
-		        $return.="\n\n\n";
-		    }
-	    }
+        $result = self::CR("SHOW TABLES IN ${db}");
 
-      //save file
-      $handle = fopen($sqlfilename,'w+');
-      fwrite($handle,$return);
-      fclose($handle);
+        //all tables
+        while($row = $result->fetch_row()){
+          $tables[]= $row[0];
+        }
+
+            foreach($tables as $table){
+                $result = self::CR("SELECT * FROM ${db}.${table}");
+                $num_fields = $result->field_count;
+                
+                $return.="-- --------------------------------------------------------\n";
+                $return.="-- Estructura de tabla para la tabla  ${table}\n";
+                $return.="-- --------------------------------------------------------\n\n";
+
+                $return.= "DROP TABLE IF EXISTS ${table};";
+                $qr=self::CR("SHOW CREATE TABLE ${db}.${table}");
+                $row2 = $qr->fetch_row();
+                $return.= "\n\n".$row2[1].";\n\n";
+              
+                for ($i = 0; $i < $num_fields; $i++) {
+                  while($row = $result->fetch_row()){
+                    $return.= "INSERT INTO ${table} VALUES(";
+                    for($j=0; $j < $num_fields; $j++) {
+                      $row[$j] = addslashes($row[$j]);
+                      $row[$j] = preg_replace("#\n#","\\n",$row[$j]);
+                      if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+                      if ($j < ($num_fields-1)) { $return.= ','; }
+                    }
+                    $return.= ");\n";
+                  }
+                }
+
+            }
+
+        //save file
+        $return.="\n\n\n";
+        $handle = fopen(sprintf($filename,$db),'w+');
+        fwrite($handle,$return);
+        fclose($handle);
+      }
 
       return true;
     }
